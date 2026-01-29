3f2b30fdab34a3bca89ad06947496129601a55d6
gobin <robinada@gmx.de>
2026-01-29 19:04:06 +0100

    addition

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+int addition(int var1, int var2) {
+    int result = var1 + var2;
+    return result;
+}
+
 int main(void) {
-    puts("Hello, CMake!");
+
+    int var1;
+    int var2;
+
+    printf("Addition\n");
+
+    printf("Geben sie die erste Zahl ein: \n");
+    scanf("%d", &var1);
+    printf("Geben sie die zweite Zahl ein: \n");
+    scanf("%d", &var2);
+
+    int result = addition(var1, var2);
+
+    printf("Ergebnis: %d\n", result);
+
     return 0;
 }
